4884c8269e95b92b01e7f033653c67bae1c3fccc
Shivansh <shivanshkumar760@gmail.com>
2022-10-06 15:34:39 +0530

    first commit

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,4 @@
+print("Hello World!", end="")  # end is given to continue in the same line
+print(" welcome to world of python ")
+print("lets get started")
+print("begin")
---
+++ voice.py
@@ -0,0 +1,4 @@
+import pyttsx3
+a=pyttsx3.init()
+a.say("hello world")
+a.runAndWait()
